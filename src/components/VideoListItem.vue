ecc79a0106f37ed14714cf12f1ef0d0c28a346c6
moonthree <fpdhslr0725@gmail.com>
2022-11-10 11:42:03 +0900

    a

---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="border m-3">
+    <img :src="video.snippet.thumbnails.default.url" alt="">
+    {{ video.snippet.title }}
+  </div>
+</template>
+
+<script>
+export default {
+  name:'VideoListItem',
+  props: {
+    video: Object,
+  },
+  created() {
+    console.log(this.video.snippet)
+  }
+}
+</script>
+
+<style>
+
+</style>
